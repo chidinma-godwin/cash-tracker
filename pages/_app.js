779a41fa649332bd900782ca807c5dc929f2e2cb
Chidinma Godwin <gchidex08@gmail.com>
2020-12-06 21:28:17 +0100

    Setup environment

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
-import '../styles/global.css'
+import '../styles/global.css';
 
 // This default export is required in a new `pages/_app.js` file.
+// eslint-disable-next-line react/prop-types
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
